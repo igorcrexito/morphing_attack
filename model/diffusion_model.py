d41071b4f5fd83335ccdfb6d5f4b9955dda501f0
igorcrexito <igorcrexito@gmail.com>
2026-06-24 01:40:19 -0300

    changing diffusion model

--- model/diffusion_model.py
+++ model/diffusion_model.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import (Input, Conv2D, BatchNormalization, Activation, MaxPooling2D, UpSampling2D, Concatenate)
 import numpy as np
+from tensorflow.keras.layers import Lambda
 
 class DiffusionModel:
 
@@ -25,9 +26,11 @@ class DiffusionModel:
         d3 = self._decoder_block(d2, s2, 64)
         d4 = self._decoder_block(d3, s1, 32)
 
-        output = Conv2D(3, 1, activation="sigmoid")(d4)
+        #output = Conv2D(3, 1, activation="sigmoid")(d4)
+        delta = Conv2D(3, 1, activation="tanh", name="residual_delta")(d4)
 
-        return Model(inputs, output)
+        return Model(inputs, delta)
+        #return Model(inputs, output)
 
 
     def _conv_block(self, x, filters):
@@ -58,7 +61,9 @@ class DiffusionModel:
         X = tf.concat([image_A, image_B, heatmap_A, heatmap_B], axis=-1)
 
         with tf.GradientTape() as tape:
-            morph = self.model(X, training=True)
+            #morph = self.model(X, training=True)
+            delta = self.model(X, training=True)
+            morph = self._build_morph(image_A, delta, heatmap_B)
 
             loss = self._balanced_loss(morph, image_A, image_B, heatmap_A, heatmap_B, alpha)
 
@@ -67,15 +72,41 @@ class DiffusionModel:
 
         return loss
 
+    def _build_morph(self, image_A, delta, heatmap_B):
+        maskB = tf.reduce_max(heatmap_B, axis=-1, keepdims=True)
+        maskB = tf.nn.avg_pool2d(maskB, ksize=11, strides=1, padding="SAME")
+
+        maskB = tf.clip_by_value(maskB, 0.0, 1.0)
+        morph = image_A + (0.20 * delta * maskB)
+
+        morph = tf.clip_by_value(morph, 0.0, 1.0)
+        return morph
+
 
     def _balanced_loss(self, morph, image_A, image_B, heatmap_A, heatmap_B, alpha):
-        maskA = tf.reduce_max(heatmap_A, axis=-1, keepdims=True)
+
         maskB = tf.reduce_max(heatmap_B, axis=-1, keepdims=True)
+        maskB = tf.nn.avg_pool2d(maskB, ksize=11, strides=1, padding="SAME")
+        maskB = tf.clip_by_value(maskB, 0.0, 1.0)
 
-        lossA = tf.reduce_mean(maskA * tf.square(morph - image_A))
-        lossB = tf.reduce_mean(maskB * tf.square(morph - image_B))
+        background_mask = 1.0 - maskB
 
-        return (alpha * lossA + (1 - alpha) * lossB)
+        target_landmark = (alpha * image_A + (1.0 - alpha) * image_B)
+
+        # global preservation of A
+        loss_recon_A = tf.reduce_mean(tf.square(morph - image_A))
+
+        # preserve outside landmarks
+        loss_background = tf.reduce_mean(background_mask * tf.square(morph - image_A))
+
+        # inject B only on landmarks
+        loss_landmark = tf.reduce_mean(maskB * tf.square(morph - target_landmark))
+        loss_shape = self._shape_loss(morph, image_A)
+        loss_color = self._color_loss(morph,image_A)
+
+        loss = (5.0 * loss_recon_A + 10.0 * loss_background + 3.0 * loss_landmark + 2.0 * loss_shape + 2.0 * loss_color)
+
+        return loss
 
 
     def fit(self, image_A, image_B, heatmap_A, heatmap_B, alpha, epochs, batch_size):
@@ -96,8 +127,27 @@ class DiffusionModel:
 
             print(f"Epoch {epoch + 1}: "f"{np.mean(epoch_loss):.4f}")
 
+
     def predict(self, image_A, image_B, heatmap_A, heatmap_B):
+
         X = tf.concat([image_A, image_B, heatmap_A, heatmap_B], axis=-1)
-        morph = self.model(X, training=False)
+        delta = self.model(X, training=False)
+        morph = self._build_morph(image_A, delta, heatmap_B)
 
         return morph
+
+    def _shape_loss(self, morph, image_A):
+        gx_m = morph[:, :, 1:, :] - morph[:, :, :-1, :]
+        gy_m = morph[:, 1:, :, :] - morph[:, :-1, :, :]
+
+        gx_a = image_A[:, :, 1:, :] - image_A[:, :, :-1, :]
+        gy_a = image_A[:, 1:, :, :] - image_A[:, :-1, :, :]
+
+        return (tf.reduce_mean(tf.square(gx_m - gx_a)) + tf.reduce_mean(tf.square(gy_m - gy_a)))
+
+
+    def _color_loss(self, morph, image_A):
+        mean_morph = tf.reduce_mean(morph, axis=[1, 2], keepdims=True)
+        mean_A = tf.reduce_mean(image_A, axis=[1, 2], keepdims=True)
+
+        return tf.reduce_mean(tf.square(mean_morph - mean_A))
